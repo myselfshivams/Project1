1d3dabe52ee160ded2869c8590baa570cf64ee25
Shivam <143623476+myselfshivams@users.noreply.github.com>
2023-11-30 17:17:15 +0530

    Delete exhibition/static directory

--- exhibition/static/preloader.js
+++
@@ -1,6 +0,0 @@
-const checkElement = async (selector) => {
-    while (document.querySelector(selector) === null) {
-        await new Promise(resolve => requestAnimationFrame(resolve))
-    }
-    return document.querySelector(selector);
-};
